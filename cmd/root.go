3edd0f72f7f5cac4644873c9bf5bd0cba544c479
xiaohui.ren <xiaohui.ren@smartx.com>
2023-08-04 10:15:38 +0800

    add function checkDeletePrefix, periodicDelete and periodicGC

--- cmd/root.go
+++ cmd/root.go
@@ -59,6 +59,18 @@ func Execute() {
 		"Interval between two metric collections")
 	fatalIf(viper.BindPFlag("interval", rootCmd.PersistentFlags().Lookup("interval")))
 
+	// size
+	rootCmd.PersistentFlags().Int("max-size", 10000, "badger db max size")
+	fatalIf(viper.BindPFlag("max-size", rootCmd.PersistentFlags().Lookup("max-size")))
+	rootCmd.PersistentFlags().Int("delete-size", 100, "badger db delete size for once")
+	fatalIf(viper.BindPFlag("delete-size", rootCmd.PersistentFlags().Lookup("delete-size")))
+
+	// period
+	rootCmd.PersistentFlags().Duration("period-second", 1*time.Second, "period of badger db delete")
+	fatalIf(viper.BindPFlag("period-second", rootCmd.PersistentFlags().Lookup("period-second")))
+	rootCmd.PersistentFlags().Duration("period-minute", 5*time.Minute, "period of badger db GC")
+	fatalIf(viper.BindPFlag("period-minute", rootCmd.PersistentFlags().Lookup("period-minute")))
+
 	err := viper.ReadInConfig()
 	if err != nil {
 		var expected viper.ConfigFileNotFoundError
--- tcpmon/storage.go
+++ tcpmon/storage.go
@@ -4,19 +4,32 @@ import (
 	"fmt"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/cockroachdb/errors"
 	"github.com/dgraph-io/badger/v4"
 	boptions "github.com/dgraph-io/badger/v4/options"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 )
 
 type Datastore struct {
-	tx   chan *KVPair
-	done chan struct{}
-	db   *badger.DB
-	wait sync.WaitGroup
+	tx           chan *KVPair
+	done         chan struct{}
+	db           *badger.DB
+	wait         sync.WaitGroup
+	tickerDelete *time.Ticker
+	tickerGC     *time.Ticker
 }
+
+type periodOption struct {
+	MaxSize      int
+	DeleteSize   int
+	PeriodSecond time.Duration
+	PeriodMinute time.Duration
+}
+
+var periodOptions *periodOption
 
 func NewDatastore(initialEpoch uint64, path string) *Datastore {
 	options := badger.DefaultOptions(path).
@@ -34,14 +47,31 @@ func NewDatastore(initialEpoch uint64, path string) *Datastore {
 	log.Info().Uint64("initialEpoch", initialEpoch).Msg("datastore created")
 	tx := make(chan *KVPair, 256)
 
+	if periodOptions == nil {
+		periodOptions = &periodOption{
+			MaxSize:      viper.GetInt("max-size"),
+			DeleteSize:   viper.GetInt("delete-size"),
+			PeriodSecond: viper.GetDuration("period-second"),
+			PeriodMinute: viper.GetDuration("period-minute"),
+		}
+	}
+
 	d := &Datastore{
 		done: make(chan struct{}),
 		tx:   tx,
 		db:   db,
+		// 使用这两行make check会出错
+		// tickerDelete: time.NewTicker(periodOptions.PeriodSecond),
+		// tickerGC:     time.NewTicker(periodOptions.PeriodMinute),
+		// 使用这两行没问题
+		tickerDelete: time.NewTicker(1 * time.Second),
+		tickerGC:     time.NewTicker(5 * time.Minute),
 	}
-	d.wait.Add(1)
+	d.wait.Add(3)
 
 	go d.writer(initialEpoch)
+	go d.periodicDelete(periodOptions.MaxSize, periodOptions.DeleteSize)
+	go d.periodicGC()
 	return d
 }
 
@@ -53,6 +83,8 @@ func (d *Datastore) Tx() chan<- *KVPair {
 // Close the datastore and shutdown
 func (d *Datastore) Close() {
 	close(d.done)
+	d.tickerDelete.Stop()
+	d.tickerGC.Stop()
 	d.wait.Wait()
 }
 
@@ -99,6 +131,83 @@ func (d *Datastore) writer(initialEpoch uint64) {
 		}
 		if req.Callback != nil {
 			req.Callback(err)
+		}
+	}
+}
+
+func (d *Datastore) checkDeletePrefix(prefix string, maxSize, deleteSize int) {
+	err := d.db.Update(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.Prefix = []byte(prefix)
+		it := txn.NewIterator(opts)
+		defer it.Close()
+
+		totalCount := 0
+		for it.Rewind(); it.Valid(); it.Next() {
+			totalCount++
+		}
+
+		if totalCount > maxSize {
+			k := 0
+			for it.Rewind(); it.Valid(); it.Next() {
+				if k >= deleteSize {
+					break
+				}
+				item := it.Item()
+				key := item.Key()
+				err := txn.Delete(key)
+				if err != nil {
+					log.Warn().Err(err).Str("key", string(key)).Msg("failed to delete item")
+				} else {
+					log.Trace().Str("key", string(key)).Msg("delete old item")
+				}
+				k++
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Warn().Err(err).Msg("failed to update db")
+	}
+}
+
+func (d *Datastore) periodicDelete(maxSize, deleteSize int) {
+	log.Info().Msg("datastore periodic delete started")
+	defer func(wait *sync.WaitGroup) {
+		wait.Done()
+		log.Info().Msg("datastore periodic delete exited")
+	}(&d.wait)
+
+	maxSize, deleteSize = maxSize/3, deleteSize/3
+	for {
+		select {
+		case <-d.done:
+			return
+		case <-d.tickerDelete.C:
+			d.checkDeletePrefix(PrefixNetRecord, maxSize, deleteSize)
+			d.checkDeletePrefix(PrefixNicRecord, maxSize, deleteSize)
+			d.checkDeletePrefix(PrefixTcpRecord, maxSize, deleteSize)
+		}
+	}
+}
+
+func (d *Datastore) periodicGC() {
+	log.Info().Msg("datastore periodic GC started")
+	defer func(wait *sync.WaitGroup) {
+		wait.Done()
+		log.Info().Msg("datastore periodic GC exited")
+	}(&d.wait)
+
+	for {
+		select {
+		case <-d.done:
+			return
+		case <-d.tickerGC.C:
+		again:
+			err := d.db.RunValueLogGC(0.5)
+			if err == nil {
+				goto again
+			}
 		}
 	}
 }
