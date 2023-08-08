b752ea2bbe347ef1c806a3bd65628141a604d157
fanyang <fanyang@smartx.com>
2023-08-08 08:31:36 +0800

    monitor: adds support for collecting ss details (#33)
    * tools: add punused
    
    To find out which functions is not used
    
    * tools: add staticcheck
    
    * storage: use GetSize() when reclaiming
    
    * util: remove unused functions
    
    * storage: fix linter error ineffective break
    
    * storage: reclaiming fast return
    
    Check before creating new txn: cond fails and return
    
    * git: ignore vscode folder
    
    * monitor: add supports for ss details

--- tcpmon/cmd_ss.go
+++ tcpmon/cmd_ss.go
@@ -47,6 +47,160 @@ func ToPbState(s string) SocketState {
 	return st
 }
 
+func isRate(s string) bool {
+	switch s {
+	case "pacing_rate":
+		return true
+	case "delivery_rate":
+		return true
+	case "send":
+		return true
+	default:
+		return false
+	}
+}
+
+func setRate(m *SocketMetric, field string, value uint64) {
+	switch field {
+	case "pacing_rate":
+		m.PacingRate = value
+	case "delivery_rate":
+		m.DeliveryRate = value
+	case "send":
+		m.Send = value
+	default:
+		log.Fatal().Str("field", field).Msg("invalid field")
+	}
+}
+
+func setMetric(m *SocketMetric, field string) {
+	p := strings.IndexRune(field, ':')
+	if p == -1 {
+		log.Fatal().Str("field", field).Msg("invalid field")
+	}
+	key := field[:p]
+	valueStr := field[p+1:]
+
+	switch key {
+	case "wscale":
+		q := strings.IndexRune(valueStr, ',')
+		if q == -1 {
+			log.Fatal().Str("field", valueStr).Msg("invalid wscale")
+		}
+		m.SndWscale, _ = ParseUint32(valueStr[:q])
+		m.RcvWscale, _ = ParseUint32(valueStr[q+1:])
+		return
+	case "rto":
+		m.Rto, _ = ParseFloat64(valueStr)
+		return
+	case "rtt":
+		q := strings.IndexRune(valueStr, '/')
+		if q == -1 {
+			log.Fatal().Str("field", valueStr).Msg("invalid rtt/rttvar")
+		}
+		m.Rtt, _ = ParseFloat64(valueStr[:q])
+		m.Rttvar, _ = ParseFloat64(valueStr[q+1:])
+		return
+	case "minrtt":
+		m.Minrtt, _ = ParseFloat64(valueStr)
+		return
+	case "busy":
+		m.BusyMs, _ = ParseUint32(strings.TrimSuffix(valueStr, "ms"))
+		return
+	case "rcv_rtt":
+		m.RcvRtt, _ = ParseFloat64(valueStr)
+		return
+	case "retrans":
+		q := strings.IndexRune(valueStr, '/')
+		if q == -1 {
+			log.Fatal().Str("field", valueStr).Msg("invalid retrans")
+		}
+		m.RetransNow, _ = ParseUint32(valueStr[:q])
+		m.RetransTotal, _ = ParseUint32(valueStr[q+1:])
+		return
+	}
+
+	value, err := ParseUint32(valueStr)
+	if err != nil {
+		log.Fatal().Str("value", valueStr).Str("key", key).Err(errors.WithStack(err)).Msg("parse failed")
+	}
+	switch key {
+	case "ato":
+		m.Ato = value
+	case "mss":
+		m.Mss = value
+	case "pmtu":
+		m.Pmtu = value
+	case "rcvmss":
+		m.Rcvmss = value
+	case "advmss":
+		m.Advmss = value
+	case "cwnd":
+		m.Cwnd = value
+	case "bytes_sent":
+		m.BytesSent = value
+	case "bytes_acked":
+		m.BytesAcked = value
+	case "bytes_received":
+		m.BytesReceived = value
+	case "data_segs_out":
+		m.DataSegsOut = value
+	case "data_segs_in":
+		m.DataSegsIn = value
+	case "segs_out":
+		m.SegsOut = value
+	case "segs_in":
+		m.SegsIn = value
+	case "lastsnd":
+		m.Lastsnd = value
+	case "lastrcv":
+		m.Lastrcv = value
+	case "lastack":
+		m.Lastack = value
+	case "delivered":
+		m.Delivered = value
+	case "rcv_space":
+		m.RcvSpace = value
+	case "rcv_ssthresh":
+		m.RcvSsthresh = value
+	case "snd_wnd":
+		m.SndWnd = value
+	}
+}
+
+func parseTimerOrMeminfo(m *SocketMetric, s string) {
+	p := strings.Index(s, ":(")
+	if p == -1 {
+		log.Fatal().Str("field", s).Msg("parse failed")
+	}
+
+	name := s[:p]
+	fields := strings.FieldsFunc(s[p+2:len(s)-1], func(r rune) bool {
+		return ',' == r
+	})
+	if name == "skmem" {
+		skmem := SocketMemoryUsage{}
+		skmem.RmemAlloc, _ = ParseUint32(strings.TrimPrefix(fields[0], "r"))
+		skmem.RcvBuf, _ = ParseUint32(strings.TrimPrefix(fields[1], "rb"))
+		skmem.WmemAlloc, _ = ParseUint32(strings.TrimPrefix(fields[2], "t"))
+		skmem.SndBuf, _ = ParseUint32(strings.TrimPrefix(fields[3], "tb"))
+		skmem.FwdAlloc, _ = ParseUint32(strings.TrimPrefix(fields[4], "f"))
+		skmem.WmemQueued, _ = ParseUint32(strings.TrimPrefix(fields[5], "w"))
+		skmem.OptMem, _ = ParseUint32(strings.TrimPrefix(fields[6], "o"))
+		skmem.BackLog, _ = ParseUint32(strings.TrimPrefix(fields[7], "bl"))
+		skmem.SockDrop, _ = ParseUint32(strings.TrimPrefix(fields[8], "d"))
+		m.Skmem = &skmem
+	} else if name == "timer" {
+		t := TimerInfo{}
+		t.Name = fields[0]
+		if len(fields) == 3 {
+			t.ExpireTimeSec, _ = ParseUint32(strings.TrimSuffix(fields[1], "sec"))
+			t.Retrans, _ = ParseUint32(fields[2])
+		}
+		m.Timers = append(m.Timers, &t)
+	}
+}
+
 func ParseSSOutput(t *TcpMetric, out []string) {
 	for _, line := range out {
 		fields := strings.FieldsFunc(line, func(c rune) bool {
@@ -61,6 +215,34 @@ func ParseSSOutput(t *TcpMetric, out []string) {
 		s.SendQ = n
 		s.LocalAddr = fields[3]
 		s.PeerAddr = fields[4]
+
+		var lastRateName string
+		for _, field := range fields[5:] {
+			switch field {
+			case "ts":
+				s.Ts = true
+			case "sack":
+				s.Sack = true
+			case "cubic":
+				s.Cubic = true
+			case "app_limited":
+				s.AppLimited = true
+			default:
+				// rate handling: pacing_rate, delivery_rate and send
+				if isRate(field) {
+					lastRateName = field
+				} else if lastRateName != "" && strings.HasSuffix(field, "bps") {
+					rate, _ := ParseUint64(strings.TrimSuffix(field, "bps"))
+					setRate(&s, lastRateName, rate)
+				} else if strings.Contains(field, ":(") {
+					// timers
+					parseTimerOrMeminfo(&s, field)
+				} else {
+					// other metrics
+					setMetric(&s, field)
+				}
+			}
+		}
 
 		t.Sockets = append(t.Sockets, &s)
 	}
--- tcpmon/storage.go
+++ tcpmon/storage.go
@@ -86,7 +86,6 @@ func (d *Datastore) writer(initialEpoch uint64) {
 		var req *KVPair
 		select {
 		case req = <-d.tx:
-			break
 		case <-d.done:
 			return
 		}
@@ -118,7 +117,7 @@ func (d *Datastore) writer(initialEpoch uint64) {
 }
 
 func (d *Datastore) GetSize() int {
-	count := 0
+	size := 0
 	err := d.db.View(func(txn *badger.Txn) error {
 		opts := badger.DefaultIteratorOptions
 		opts.PrefetchValues = false
@@ -126,7 +125,7 @@ func (d *Datastore) GetSize() int {
 		defer it.Close()
 
 		for it.Rewind(); it.Valid(); it.Next() {
-			count++
+			size++
 		}
 		return nil
 	})
@@ -134,10 +133,15 @@ func (d *Datastore) GetSize() int {
 		log.Fatal().Err(errors.WithStack(err)).Msg("get size failed")
 	}
 
-	return count
+	return size
 }
 
 func (d *Datastore) checkDeletePrefix(prefix []byte, maxSize, deleteSize int) {
+	size := d.GetSize()
+	if size <= maxSize {
+		return
+	}
+
 	err := d.db.Update(func(txn *badger.Txn) error {
 		opts := badger.DefaultIteratorOptions
 		opts.Prefix = prefix
@@ -146,26 +150,19 @@ func (d *Datastore) checkDeletePrefix(prefix []byte, maxSize, deleteSize int) {
 		it := txn.NewIterator(opts)
 		defer it.Close()
 
-		count := 0
+		deleted := 0
 		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
-			count++
-		}
-
-		if count > maxSize {
-			deleted := 0
-			for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
-				if deleted >= deleteSize || deleted >= count-maxSize {
-					break
-				}
-
-				item := it.Item()
-				key := item.KeyCopy(nil)
-				err := txn.Delete(key)
-				if err != nil {
-					log.Warn().Err(err).Str("key", string(key)).Msg("failed to delete item")
-				}
-				deleted++
+			if deleted >= deleteSize || deleted >= size-maxSize {
+				break
 			}
+
+			item := it.Item()
+			key := item.KeyCopy(nil)
+			err := txn.Delete(key)
+			if err != nil {
+				log.Warn().Err(err).Str("key", string(key)).Msg("failed to delete item")
+			}
+			deleted++
 		}
 
 		return nil
