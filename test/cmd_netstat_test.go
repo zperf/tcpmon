6ae5ccb89fd2cd13d3de2c4cf37f1c312315a055
fanyang <fanyang@smartx.com>
2023-08-23 16:01:02 +0800

    monitor: fix netstat stat overflow
    The fields are all %llu
    
    https://github.com/ecki/net-tools/blob/master/statistics.c

--- test/cmd_netstat_test.go
+++ test/cmd_netstat_test.go
@@ -13,108 +13,141 @@ func (s *CommandParserTestSuite) TestParseNetstat() {
 	assert := s.Assert()
 
 	out := `Ip:
-    1066283 total packets received
+    12984693843 total packets received
+    2 with invalid headers
+    13 with invalid addresses
     0 forwarded
     0 incoming packets discarded
-    1061338 incoming packets delivered
-    917653 requests sent out
-    16 outgoing packets dropped
-    2 reassemblies required
-    1 packets reassembled ok
+    12975893623 incoming packets delivered
+    11393863778 requests sent out
+    2960 outgoing packets dropped
+    776 dropped because of missing route
+    12 reassemblies required
+    6 packets reassembled ok
+    184 fragments received ok
+    368 fragments created
 Icmp:
-    36 ICMP messages received
-    0 input ICMP message failed.
+    140154534 ICMP messages received
+    14186 input ICMP message failed.
     ICMP input histogram:
-        destination unreachable: 34
-        echo requests: 2
-    39 ICMP messages sent
+        destination unreachable: 48182
+        redirects: 77
+        echo requests: 70633563
+        echo replies: 69472712
+    140181726 ICMP messages sent
     0 ICMP messages failed
     ICMP output histogram:
-        destination unreachable: 37
-        echo replies: 2
+        destination unreachable: 48265
+        echo request: 69499898
+        echo replies: 70633563
 IcmpMsg:
-        InType3: 34
-        InType8: 2
-        OutType0: 2
-        OutType3: 37
+        InType0: 69472712
+        InType3: 48182
+        InType5: 77
+        InType8: 70633563
+        OutType0: 70633563
+        OutType3: 48265
+        OutType8: 69499898
 Tcp:
-    566 active connections openings
-    81 passive connection openings
-    120 failed connection attempts
-    9 connection resets received
-    5 connections established
-    1059991 segments received
-    944977 segments send out
-    2328 segments retransmited
-    0 bad segments received.
-    256 resets sent
+    49845605 active connections openings
+    46253346 passive connection openings
+    6129349 failed connection attempts
+    2184394 connection resets received
+    892 connections established
+    12596717510 segments received
+    31113950007 segments send out
+    48107538 segments retransmited
+    27 bad segments received.
+    11229045 resets sent
 Udp:
-    1277 packets received
-    34 packets to unknown port received.
-    0 packet receive errors
-    1352 packets sent
-    0 receive buffer errors
+    196786471 packets received
+    6480 packets to unknown port received.
+    68957158 packet receive errors
+    265752378 packets sent
+    68957158 receive buffer errors
     0 send buffer errors
+    IgnoredMulti: 4363047
 UdpLite:
 TcpExt:
-    282 TCP sockets finished time wait in fast timer
-    74 packets rejects in established connections because of timestamp
-    33878 delayed acks sent
-    6 delayed acks further delayed because of locked socket
-    Quick ack mode was activated 892 times
-    26 packets directly queued to recvmsg prequeue.
-    2404 bytes directly received in process context from prequeue
-    406412 packet headers predicted
-    2 packets header predicted and directly queued to user
-    91197 acknowledgments not containing data payload received
-    238951 predicted acknowledgments
-    227 times recovered from packet loss by selective acknowledgements
-    Detected reordering 1 times using SACK
-    Detected reordering 3 times using time stamp
-    3 congestion windows partially recovered using Hoe heuristic
-    131 congestion windows recovered without slow start by DSACK
-    138 congestion windows recovered without slow start after partial ack
-    202 timeouts after SACK recovery
-    2 timeouts in loss state
-    324 fast retransmits
-    15 forward retransmits
-    48 retransmits in slow start
-    209 other TCP timeouts
-    TCPLossProbes: 837
-    TCPLossProbeRecovery: 558
-    16 SACK retransmits failed
-    950 DSACKs sent for old packets
-    12 DSACKs sent for out of order packets
-    627 DSACKs received
-    29 DSACKs for out of order packets received
-    12 connections reset due to unexpected data
-    4 connections reset due to early user close
-    128 connections aborted due to timeout
-    TCPDSACKIgnoredOld: 1
-    TCPDSACKIgnoredNoUndo: 177
-    TCPSpuriousRTOs: 62
-    TCPSackShifted: 24
-    TCPSackMerged: 232
-    TCPSackShiftFallback: 1304
-    IPReversePathFilter: 1
-    TCPRcvCoalesce: 83625
-    TCPOFOQueue: 73474
-    TCPOFOMerge: 18
-    TCPAutoCorking: 2273
-    TCPSynRetrans: 827
-    TCPOrigDataSent: 484717
-    TCPHystartDelayDetect: 4
-    TCPHystartDelayCwnd: 608
+    70 invalid SYN cookies received
+    20 resets received for embryonic SYN_RECV sockets
+    27 ICMP packets dropped because they were out-of-window
+    52061656 TCP sockets finished time wait in fast timer
+    7 time wait sockets recycled by time stamp
+    125745 packets rejects in established connections because of timestamp
+    112076710 delayed acks sent
+    158300 delayed acks further delayed because of locked socket
+    Quick ack mode was activated 9998003 times
+    981 times the listen queue of a socket overflowed
+    981 SYNs to LISTEN sockets dropped
+    7168650378 packet headers predicted
+    1890902648 acknowledgments not containing data payload received
+    3592506430 predicted acknowledgments
+    5453091 times recovered from packet loss by selective acknowledgements
+    Detected reordering 3544733 times using SACK
+    Detected reordering 624427 times using time stamp
+    55620 congestion windows fully recovered without slow start
+    612208 congestion windows partially recovered using Hoe heuristic
+    356499 congestion windows recovered without slow start by DSACK
+    7545 congestion windows recovered without slow start after partial ack
+    TCPLostRetransmit: 883886
+    1203 timeouts after SACK recovery
+    878 timeouts in loss state
+    44563971 fast retransmits
+    798742 retransmits in slow start
+    27969 other TCP timeouts
+    TCPLossProbes: 5213051
+    TCPLossProbeRecovery: 41798
+    105391 SACK retransmits failed
+    TCPBacklogCoalesce: 104127568
+    10483485 DSACKs sent for old packets
+    82041 DSACKs sent for out of order packets
+    4482837 DSACKs received
+    3902 DSACKs for out of order packets received
+    1010427 connections reset due to unexpected data
+    2444880 connections reset due to early user close
+    3215 connections aborted due to timeout
+    TCPDSACKIgnoredOld: 241397
+    TCPDSACKIgnoredNoUndo: 2161962
+    TCPSpuriousRTOs: 148
+    TCPSackShifted: 4552047
+    TCPSackMerged: 19855226
+    TCPSackShiftFallback: 51614523
+    IPReversePathFilter: 33206
+    TCPRcvCoalesce: 756764461
+    TCPOFOQueue: 171697744
+    TCPOFOMerge: 105963
+    TCPChallengeACK: 113666
+    TCPSYNChallenge: 30
+    TCPSpuriousRtxHostQueues: 119227
+    TCPAutoCorking: 110895312
+    TCPFromZeroWindowAdv: 30453
+    TCPToZeroWindowAdv: 30453
+    TCPWantZeroWindowAdv: 1192398
+    TCPSynRetrans: 23366
+    TCPOrigDataSent: 27644619399
+    TCPHystartTrainDetect: 700103
+    TCPHystartTrainCwnd: 18378617
+    TCPHystartDelayDetect: 6798
+    TCPHystartDelayCwnd: 660016
+    TCPACKSkippedSynRecv: 1303
+    TCPACKSkippedPAWS: 103693
+    TCPACKSkippedSeq: 38587
+    TCPACKSkippedFinWait2: 201
+    TCPACKSkippedTimeWait: 184
+    TCPACKSkippedChallenge: 65148
+    TCPWinProbe: 1085
+    TCPKeepAlive: 55886023
+    TCPDelivered: 27689076642
+    TCPAckCompressed: 101435862
 IpExt:
-    InNoRoutes: 2
-    InMcastPkts: 2
-    InBcastPkts: 4355
-    InOctets: 587672631
-    OutOctets: 228218297
-    InMcastOctets: 72
-    InBcastOctets: 673050
-    InNoECTPkts: 1021973
-    InECT0Pkts: 44310`
+    InBcastPkts: 4363057
+    InOctets: 84129285172077
+    OutOctets: 67110093964178
+    InBcastOctets: 1400581363
+    InNoECTPkts: 43749298150
+    InECT0Pkts: 13952
+`
 
 	lines := strings.FieldsFunc(out, func(r rune) bool {
 		return r == '\n'
@@ -125,28 +158,28 @@ IpExt:
 	r.Type = MetricType_NET
 	ParseNetstatOutput(&r, lines)
 
-	assert.Equal(uint32(1066283), r.IpTotalPacketsReceived)
-	assert.Equal(uint32(0), r.IpForwarded)
-	assert.Equal(uint32(0), r.IpIncomingPacketsDiscarded)
-	assert.Equal(uint32(1061338), r.IpIncomingPacketsDelivered)
-	assert.Equal(uint32(917653), r.IpRequestsSentOut)
-	assert.Equal(uint32(16), r.IpOutgoingPacketsDropped)
+	assert.Equal(uint64(12984693843), r.IpTotalPacketsReceived)
+	assert.Equal(uint64(0), r.IpForwarded)
+	assert.Equal(uint64(0), r.IpIncomingPacketsDiscarded)
+	assert.Equal(uint64(12975893623), r.IpIncomingPacketsDelivered)
+	assert.Equal(uint64(11393863778), r.IpRequestsSentOut)
+	assert.Equal(uint64(2960), r.IpOutgoingPacketsDropped)
 
-	assert.Equal(uint32(566), r.TcpActiveConnectionsOpenings)
-	assert.Equal(uint32(81), r.TcpPassiveConnectionOpenings)
-	assert.Equal(uint32(120), r.TcpFailedConnectionAttempts)
-	assert.Equal(uint32(9), r.TcpConnectionResetsReceived)
-	assert.Equal(uint32(5), r.TcpConnectionsEstablished)
-	assert.Equal(uint32(1059991), r.TcpSegmentsReceived)
-	assert.Equal(uint32(944977), r.TcpSegmentsSendOut)
-	assert.Equal(uint32(2328), r.TcpSegmentsRetransmitted)
-	assert.Equal(uint32(0), r.TcpBadSegmentsReceived)
-	assert.Equal(uint32(256), r.TcpResetsSent)
+	assert.Equal(uint64(49845605), r.TcpActiveConnectionsOpenings)
+	assert.Equal(uint64(46253346), r.TcpPassiveConnectionOpenings)
+	assert.Equal(uint64(6129349), r.TcpFailedConnectionAttempts)
+	assert.Equal(uint64(2184394), r.TcpConnectionResetsReceived)
+	assert.Equal(uint64(892), r.TcpConnectionsEstablished)
+	assert.Equal(uint64(12596717510), r.TcpSegmentsReceived)
+	assert.Equal(uint64(31113950007), r.TcpSegmentsSendOut)
+	assert.Equal(uint64(48107538), r.TcpSegmentsRetransmitted)
+	assert.Equal(uint64(27), r.TcpBadSegmentsReceived)
+	assert.Equal(uint64(11229045), r.TcpResetsSent)
 
-	assert.Equal(uint32(1277), r.UdpPacketsReceived)
-	assert.Equal(uint32(34), r.UdpPacketsToUnknownPortReceived)
-	assert.Equal(uint32(0), r.UdpPacketReceiveErrors)
-	assert.Equal(uint32(1352), r.UdpPacketsSent)
-	assert.Equal(uint32(0), r.UdpReceiveBufferErrors)
-	assert.Equal(uint32(0), r.UdpSendBufferErrors)
+	assert.Equal(uint64(196786471), r.UdpPacketsReceived)
+	assert.Equal(uint64(6480), r.UdpPacketsToUnknownPortReceived)
+	assert.Equal(uint64(68957158), r.UdpPacketReceiveErrors)
+	assert.Equal(uint64(265752378), r.UdpPacketsSent)
+	assert.Equal(uint64(68957158), r.UdpReceiveBufferErrors)
+	assert.Equal(uint64(0), r.UdpSendBufferErrors)
 }
