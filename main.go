80eaf3158d641ab594c85dff88547210c1d32c4f
xiaohui.ren <xiaohui.ren@smartx.com>
2023-09-20 15:59:20 +0800

    main: add debug.SetMemoryLimit

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"os"
+	"runtime/debug"
 	"strings"
 
 	"github.com/zperf/tcpmon/cmd"
 )
 
 func main() {
+	debug.SetMemoryLimit(100 * 1024 * 1024)
 	cmd.Execute(strings.Join(os.Args[1:], " "))
 }
